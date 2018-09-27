de13df13f88baf6a7f17f04ff7eb10654a20e702
ChristianTaidi <christiantaidi@gmail.com>
2018-09-27 16:54:51 +0200

    fail due to incompatibility of awt and android

--- app/src/main/java/com/example/group6/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/group6/spaceinvaders/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.christian.spaceinvaders;
+package com.example.group6.spaceinvaders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.christian.spaceinvaders.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/group6/spaceinvaders/core/model/SpaceShip.java
+++ app/src/main/java/com/example/group6/spaceinvaders/core/model/SpaceShip.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import Engine.GameObject;
 import Engine.Vector2;
 
+
 /**
  *
  * Created by Christian on 25/09/2018.
@@ -21,7 +22,9 @@ public class SpaceShip extends GameObject {
         this.setSize(width,height);
         this.setColor(color);
         this.setCenter(center);
+
     }
+
 
     private void setColor(Color color){
         this.color= color;
